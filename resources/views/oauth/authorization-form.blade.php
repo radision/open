9c964a0e5d96127b947965684a8941f9f242286d
radision <radision@126.com>
2016-05-18 18:42:45 +0800

    login page

--- resources/views/oauth/authorization-form.blade.php
+++ resources/views/oauth/authorization-form.blade.php
@@ -1,12 +1,37 @@
-<h2>{{$client->getName()}}</h2>
-<form method="post" action="{{route('oauth.authorize.post', $params)}}">
-  {{ csrf_field() }}
-  <input type="hidden" name="client_id" value="{{$params['client_id']}}">
-  <input type="hidden" name="redirect_uri" value="{{$params['redirect_uri']}}">
-  <input type="hidden" name="response_type" value="{{$params['response_type']}}">
-  <input type="hidden" name="state" value="{{$params['state']}}">
-  <input type="hidden" name="scope" value="{{$params['scope']}}">
+@extends('layouts.master')
 
-  <button type="submit" name="approve" value="1">Approve</button>
-  <button type="submit" name="deny" value="1">Deny</button>
-</form>
+@section('content')
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">欢迎访问 {{$client->getName()}}</h3>
+  </div>
+  <div class="panel-body">
+    <form class="form-horizontal" method="post" action="{{route('oauth.authorize.post', $params)}}">
+      {{ csrf_field() }}
+      <input type="hidden" name="client_id" value="{{$params['client_id']}}">
+      <input type="hidden" name="redirect_uri" value="{{$params['redirect_uri']}}">
+      <input type="hidden" name="response_type" value="{{$params['response_type']}}">
+      <input type="hidden" name="state" value="{{$params['state']}}">
+      <input type="hidden" name="scope" value="{{$params['scope']}}">
+
+      <div class="form-group">
+        <label for="mobile" class="col-sm-2 control-label">手机号</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="mobile" name="mobile" placeholder="手机号">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="passwd" class="col-sm-2 control-label">密码</label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="passwd" name="passwd" placeholder="密码">
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <button class="btn btn-success" type="submit" name="approve" value="1">授权</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+@endsection
