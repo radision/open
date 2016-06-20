71a4141956262579d49be09b3574d68ec02a2072
sun.xianwei <sun.xianwei@xcar.com.cn>
2016-06-20 19:21:30 +0800

    reset auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,31 +5,27 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">登录</div>
+                <div class="panel-heading">Login</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        <input type="hidden" name="response_type" value="{{ app('request')->input('response_type') }}">
-                        <input type="hidden" name="client_id" value="{{ app('request')->input('client_id') }}">
-                        <input type="hidden" name="state" value="{{ app('request')->input('state') }}">
-                        <input type="hidden" name="redirect_uri" value="{{ app('request')->input('redirect_uri') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('mobile') ? ' has-error' : '' }}">
-                            <label for="mobile" class="col-md-4 control-label">手机号</label>
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="mobile" type="text" class="form-control" name="mobile" value="{{ old('mobile') }}">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
 
-                                @if ($errors->has('mobile'))
+                                @if ($errors->has('email'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('mobile') }}</strong>
+                                        <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">密码</label>
+                            <label for="password" class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password">
@@ -46,7 +42,7 @@
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember" disabled> Remember Me
+                                        <input type="checkbox" name="remember"> Remember Me
                                     </label>
                                 </div>
                             </div>
@@ -55,9 +51,10 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i> 登录
+                                    <i class="fa fa-btn fa-sign-in"></i> Login
                                 </button>
 
+                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
                             </div>
                         </div>
                     </form>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">欢迎使用</div>
+                <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
-                    <a href="/home" class="btn btn-success">登录</a>
+                    Your Application's Landing Page.
                 </div>
             </div>
         </div>
